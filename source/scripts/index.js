0f8d1414c1d88f4847a5168dd214daa356c7394c
CarinaMir <67471854+CarinaMir@users.noreply.github.com>
2024-05-21 11:44:33 +0300

    Module6-task1 (#6)
    * Работы по блоку 6
    
    * Доработки по мобильной части
    
    * Доработки по мобильной версии
    
    * Начало работ по адоптации для планшета
    
    * Исправление замечаний по блоку 6
    
    * Исправление замечаний по блоку 6.
    
    * Исправлена помарка
    
    * Исправлена помарка
    
    * Исправление помарок
    
    * Работы по блоку адоптация
    
    * Удален лишний пробел
    
    * Доработы по блоку Как это работает и Живой пример
    
    * Доработки по планшетной версии
    
    * Небольшие правки
    
    * Исправление замечаний по блоку 7
    
    * Исправление замечаний по блоку 7
    
    * Исправление замечаний по блоку 7
    
    * Заменена цвета
    
    ---------
    
    Co-authored-by: Karina <alt.k@yandex.ru>

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,13 @@
-/* в этот файл добавляет скрипты*/
+const navToggle = document.querySelector('.header__toggle');
+const navBottonMenu = document.querySelector('.header__toggle--closed');
+const navMain = document.querySelector('.navigation');
+
+navToggle.addEventListener('click', () => {
+  if (navBottonMenu.classList.contains('header__toggle--closed')) {
+    navBottonMenu.classList.remove('header__toggle--closed');
+    navMain.classList.add('navigation--active');
+  } else {
+    navBottonMenu.classList.add('header__toggle--closed');
+    navMain.classList.remove('navigation--active');
+  }
+});
