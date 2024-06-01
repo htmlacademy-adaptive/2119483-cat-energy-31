1b8d79e9ac3175b57e4355ec5ab9cbff5e148458
CarinaMir <67471854+CarinaMir@users.noreply.github.com>
2024-06-01 12:36:46 +0300

    Module7-task1 (#7)
    * Работы по блоку 7
    
    * Добавлена карта
    
    * Доработки по блоку index
    
    * Работы по секции Каталог мобильная верстка
    
    * Доработки по планшетной верстке
    
    * Удален лишний класс у страницы catalog
    
    * Испарвление ошибок по мобильной верстке старницы Catalog
    
    * Исправление замечаний по мобильной верстке catalog
    
    * Доработки по планшетной версии
    
    * Завершающие работы по верстке планшетной версии
    
    * Начало верстки для версии ПК
    
    * Верстка версии для ПК
    
    * PerfectPixel верстка index Mobile
    
    * Perfect Pixel для mobile корректировки
    
    * Perfect Pixel корректировка для mobile и Tablet
    
    * PerfectPixel для desktop
    
    * Исправление части замечаний после проверки ДЗ
    
    * Правки по верстке Mobile index
    
    * Исправление замечаний по Index
    
    * Верстка perfect Pixel для catalog Mobile
    
    * PerfectPixel верстка для catalog Tablet
    
    * Доделка по tablet catalog
    
    * Desktop catalog  - внесение испавлений
    
    * Сорректировано смещение
    
    * slider_bottom скорретирован padding
    
    * Добавлен атрибут закгрузки lazy для файлов img. Добавлены артибуты разметки для автотестирования.
    
    * Предзагрузка шрифтов
    
    * Предзагрузка грифтов и стилей на странице catalog и index
    
    * Внесение изменений по критериям проекта
    
    * Доработки по styleguide
    
    * Замена наименований цветов с hex на переменные
    
    * Переделаны кнопки в блоке Каталог
    
    * Добавлены ссылки переходы на страницы-детализации
    
    * Исправлен цвет ссылки у HtmlAcademy, внесены корректировки в страницу Catalog
    
    * Добавлены ссылки скартинок товаров
    
    * По умолчанию меню закрыто
    
    * urls рефакторинг
    
    * Изменен вид добавления иконки htmlAcademy в footer
    
    * Правки стилей
    
    * Исправление ошибок на странице index
    
    * Правки по стилям
    
    * Исправлен Исправлено поведение для мобильной версии
    
    ---------
    
    Co-authored-by: Karina <alt.k@yandex.ru>

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,13 +1,15 @@
 const navToggle = document.querySelector('.header__toggle');
-const navBottonMenu = document.querySelector('.header__toggle--closed');
 const navMain = document.querySelector('.navigation');
 
+document.querySelector('.header--no-js').classList.remove('header--no-js');
+document.querySelector('.contact__map--no-js').classList.remove('contact__map--no-js');
+
 navToggle.addEventListener('click', () => {
-  if (navBottonMenu.classList.contains('header__toggle--closed')) {
-    navBottonMenu.classList.remove('header__toggle--closed');
+  if (navToggle.classList.contains('header__toggle--closed')) {
+    navToggle.classList.remove('header__toggle--closed');
     navMain.classList.add('navigation--active');
   } else {
-    navBottonMenu.classList.add('header__toggle--closed');
+    navToggle.classList.add('header__toggle--closed');
     navMain.classList.remove('navigation--active');
   }
 });
